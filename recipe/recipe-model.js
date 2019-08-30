67ff9e426c351fd6957687456934785d340058b7
sam-malanchuk <malanchuksam@gmail.com>
2019-08-29 21:32:11 -0400

    saving unfinished api endpoint to get the ingredients of a recipe by ID

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -11,7 +11,12 @@ function getRecipes() {
 }
 
 function getShoppingList(recipe_id) {
-
+  return db.select('r.quantity').from('recipe_ingredients as r')
+    // .select('r.id', 'r.name', 'ri_quantity')
+    // .from('recipes as r')
+    // .join('recipe_ingredients as ri', 'r.id', 'ri.recipes_id')
+    .join('ingredients as i', 'i.ingredients_id', 'r.id')
+    .where({'r.recipe_id': recipe_id});
 }
 
 function getInstructions(recipe_id) {
--- recipe/recipe-router.js
+++ recipe/recipe-router.js
@@ -5,13 +5,25 @@ const Recipes = require('./recipe-model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-//   res.status(200).json('You reached the recipes endpoint')
   Recipes.getRecipes()
     .then(recipes => {
         res.status(200).json(recipes);
     })
     .catch(err => {
         res.status(500).json({ message: 'There was a problem getting the recipes' });
+    })
+});
+
+router.get('/:id/shoppingList', (req, res) => {
+  const { id } = req.params;
+
+  Recipes.getShoppingList(id)
+    .then(recipe => {
+        console.log(recipe);
+        res.status(200).json(recipe);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'There was an error getting the recipe', error: err });
     })
 });
 
