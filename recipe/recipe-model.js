97d465bee882da34ac8e0d813f0c9a64d6ad75a0
sam-malanchuk <malanchuksam@gmail.com>
2019-08-29 21:05:26 -0400

    endpoint created to get a list of the recipes

--- recipe/recipe-model.js
+++ recipe/recipe-model.js
@@ -7,7 +7,7 @@ module.exports = {
 }
 
 function getRecipes() {
-
+  return db('recipes');
 }
 
 function getShoppingList(recipe_id) {
--- recipe/recipe-router.js
+++ recipe/recipe-router.js
@@ -5,7 +5,14 @@ const Recipes = require('./recipe-model.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).json('You reached the recipes endpoint')
+//   res.status(200).json('You reached the recipes endpoint')
+  Recipes.getRecipes()
+    .then(recipes => {
+        res.status(200).json(recipes);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'There was a problem getting the recipes' });
+    })
 });
 
 module.exports = router;
